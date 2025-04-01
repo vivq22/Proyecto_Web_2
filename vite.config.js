1dcfec8848f8c5937c36340ef40588f62122c6a9
Vanessa Valladares <100142668+vivq22@users.noreply.github.com>
2025-03-31 22:28:27 -0600

    Feat: Inicializando Repositorio

---
+++ vite.config.js
@@ -0,0 +1,56 @@
+import path, {resolve} from 'node:path'
+import { defineConfig } from 'vite'
+import * as glob from 'glob'
+import htmlPurge from 'vite-plugin-purgecss'
+import handlebars from 'vite-plugin-handlebars'
+import {ViteMinifyPlugin} from 'vite-plugin-minify'
+import { getPageContext } from './data/index'
+
+const obtenerEntradas = ()=>{
+    return Object.fromEntries(
+        [
+            ...glob.sync(
+                './**/*.html',
+                {
+                    ignore: [
+                        './dist/**',
+                        './node_modules/**'
+                    ]
+                }
+            ).map(
+                fileData => [
+                    fileData.slice(0, fileData.length - path.extname(fileData).length),
+                    resolve(__dirname, fileData)
+                ]
+            )
+        ]
+    );
+}
+
+
+export default defineConfig({
+    appType: 'mpa',
+    base: process.env.DEPLOY_BASE_URL,
+    build: {
+        rollupOptions: {
+            input: obtenerEntradas()
+        },
+        minify: true
+    },
+    plugins: [
+        handlebars({
+            partialDirectory: resolve(__dirname, 'partials'),
+            context : getPageContext()
+        }),
+        htmlPurge({})
+    ]
+});
+
+/**
+ 
+✓ 2 modules transformed.
+dist/index.html                 1.71 kB │ gzip: 0.63 kB
+dist/assets/index-DMCu4TLF.css  5.83 kB │ gzip: 1.22 kB
+✓ built in 251ms
+
+ */
